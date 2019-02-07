a8a182290f86408daaec9efe053e3908092d79a7
Thomas Fogg <tfogg2@gmail.com>
2019-02-06 23:03:45 -0600

    splash copy

--- src/components/home/Splash.js
+++ src/components/home/Splash.js
@@ -7,7 +7,7 @@ class Splash extends Component{
       <div className="splash">
         <div className="splash-copy">
           <h1>Austinites. Unite.</h1>
-          <p>I speak from experience when I say that moving to Austin can leave you overwhelmed. <b>I'm on a mission to make it easier for all of us.</b></p>
+          <p>I speak from experience when I say that moving to Austin can leave you overwhelmed. <b>I'm on a mission to make it better for all of us.</b></p>
         </div>
       </div>
     )
