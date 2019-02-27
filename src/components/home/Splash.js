c69e6d192a328c816337c9e1c723abfc649b8f6c
Thomas Fogg <tfogg2@gmail.com>
2019-02-27 02:01:11 -0600

    splash

--- src/components/home/Splash.js
+++ src/components/home/Splash.js
@@ -7,7 +7,7 @@ class Splash extends Component{
       <div className="splash">
         <div className="splash-copy">
           <h1>Austinited.</h1>
-          <p>I speak from experience when I say that moving to Austin can leave you overwhelmed. <b>I'm on a mission to make it better for all of us. </b> <br/><br/> Stay tuned for the next version of <b>Austinited.com</b> that will bring a much needed, <b>user-friendly</b>, experience to finding the best spots in <b>Austin, TX.</b></p>
+          <p>I speak from experience when I say that moving to Austin can leave you overwhelmed. <b>I'm on a mission to make it better for all of us. </b> <br/><br/> Stay tuned for the next version of <b>Austinited.com</b> that will bring a much needed <b>user-friendly</b> experience to finding the best spots in <b>Austin, TX.</b></p>
         </div>
       </div>
     )
